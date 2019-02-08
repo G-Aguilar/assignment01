cb56ac444672090f131dea389ce78f7dbfabfac9
Guillermo <memo5248@gmail.com>
2019-02-07 21:43:23 -0800

    assignment01
    assignment01

---
+++ js/script.js
@@ -0,0 +1,182 @@
+//STEP 1
+
+/*  var some_Month;
+    function the Month();
+    var current-Month;
+    var summer_Month;
+    var myLibrary-Function
+*/
+
+//STEP 2
+
+/*
+    5.4;
+    "hello";
+    false
+    null
+*/
+
+//STEP 3
+
+/*
+    var anExpression = 5 * (10 / 2) + 10;
+    var secondExpression = Math.PI * radius * radius;
+*/
+
+//STEP 4
+
+/*
+    var strFirstName, strLastName, numAddress, strCity, strState, numZipCode, numYourAge, refferralSource, mayWeContactYou;
+*/
+
+//STEP 5
+
+/*
+    var strFirstName;
+    strFirstName = "guillermo";
+    
+    var strLastName;
+    strLastName = "aguilar";
+    
+    var numAddress;
+    numAddress = 3586;
+
+    var strCity = "San Diego";
+
+    var strState = "California";
+
+    var numZipCode = 92115;
+
+    var numYourAge = 30, refferralSource = "john doe", mayWeContactYou = "yes"; 
+*/
+
+//STEP 6
+/*
+    var myAge = 30;
+    myAge = myAge + "something";
+    window.console.log(myAge);
+*/
+//STEP 7
+/*
+    var question1, question2;
+    question1 = true + "something";
+    window.console.log(question1);
+    question2 = 25 + false;
+    window.console.log(question2);
+*/
+//STEP 8
+/*
+    no this is not literal valid.
+    Add a Backslash inbetween the word Im.
+    
+    var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+    window.console.log(someString);
+*/
+
+//STEP 9
+
+/*
+    var x = 200;
+    var x = null;
+    window.console.log(x);
+    
+    var x;
+    window.console.log(x);
+ */
+
+//STEP 10
+/*    
+    typeof "guillermo"
+    typeof 8
+    typeof false
+    typeof [1,2,3,4]
+    typeof myHouse
+    */
+
+//STEP 11
+/*
+    alert("Hello" + "Guillermo" + ",welcome to the Javascript class!");
+*/
+
+//STEP 12
+/*
+    var name = "Guillermo";
+    alert("Hello" + name + ",welcome to the Javascript class!");
+*/
+
+//STEP 13
+/*
+    var course = "javaScript";
+    alert(course);
+*/
+
+//STEP 14
+/*
+    alert("Hello Zak Ruvalcaba.\nWelcome to the Javascript class!");
+*/
+
+//STEP 15
+   /* 
+    var name = window.prompt("Enter name");
+    alert("Hello" + " "+ name + ",welcome to the Javascript class!");
+    */
+    
+//STEP 16
+/*
+    var name = window.prompt("Enter name");
+    var Javascript = window.prompt("what class are you taking");
+    alert("Hello" + " " + name + ", welcome to the" + Javascript +"class!");
+*/
+//STEP 17
+/*
+    var x = 10, y = 20;
+window.console.log(x + y);
+*/
+
+//STEP 18
+/*
+    var x = 20;
+    window.console.log(x + 20);
+*/
+//STEP 19
+/*
+    var x = 20;
+   x = x * 5;
+    window.console.log(x);
+*/
+
+//STEP 20
+/*
+    var x = 20 % 3;
+    x /= 1;
+    window.console.log(x);
+*/
+//STEP 21
+/*
+    var x = 5, y = 10;
+    window.console.log(x <= y);
+    window.console.log(x == 5 && y == 10);
+*/
+
+//STEP 22
+/*
+    var x = 5, y = 10;
+    window.console.log(x > y);
+    window.console.log(x <= y != true);
+*/
+//STEP 23
+/*
+    var widget = new Object();
+    window.console.log(typeof widget);
+    */
+//STEP 24
+/*
+   var widget = new Object();
+    window.console.log(widget instanceof Object);
+*/
+
+//STEP 25
+/*
+   var widget = new Object();
+    window.console.log(widget instanceof Number);
+*/
